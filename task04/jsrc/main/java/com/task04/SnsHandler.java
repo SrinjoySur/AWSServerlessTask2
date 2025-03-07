07596c559c2f1496f5bb3a283b986ea29dfb0aa6
Srinjoy Sur <srinjoy_sur@epam.com>
2025-03-07 15:09:53 +0530

    commit1

--- task04/jsrc/main/java/com/task04/SnsHandler.java
+++ task04/jsrc/main/java/com/task04/SnsHandler.java
@@ -3,6 +3,7 @@ package com.task04;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
+import com.syndicate.deployment.annotations.lambda.SnsEventSource;
 import com.syndicate.deployment.model.RetentionSetting;
 
 import java.util.HashMap;
@@ -14,6 +15,9 @@ import java.util.Map;
 	isPublishVersion = true,
 	aliasName = "${lambdas_alias_name}",
 	logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
+)
+@SnsEventSource(
+		targetTopic = "lambda_topic"
 )
 public class SnsHandler implements RequestHandler<Object, Map<String, Object>> {
 
--- task04/jsrc/main/java/com/task04/SqsHandler.java
+++ task04/jsrc/main/java/com/task04/SqsHandler.java
@@ -3,6 +3,7 @@ package com.task04;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
+import com.syndicate.deployment.annotations.lambda.SqsTriggerEventSource;
 import com.syndicate.deployment.model.RetentionSetting;
 
 import java.util.HashMap;
@@ -14,6 +15,10 @@ import java.util.Map;
 	isPublishVersion = true,
 	aliasName = "${lambdas_alias_name}",
 	logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
+)
+@SqsTriggerEventSource(
+		targetQueue = "async_queue",
+		batchSize = 10
 )
 public class SqsHandler implements RequestHandler<Object, Map<String, Object>> {
 
